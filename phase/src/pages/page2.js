bc96646ea01ede5202b83260e26365b7fa25c55a
Nathan <nathan19@umd.edu>
2023-09-26 22:25:15 -0400

    Tab 2 and 6

--- phase/src/pages/page2.js
+++ phase/src/pages/page2.js
@@ -3,7 +3,27 @@ import React from 'react';
 const Page2 = () => {
 return (
 	<div>
-	<h1>TAB 2</h1>
+		<h1><bold>Choices</bold></h1>
+		<p>Hat color to place on Player One's head:</p>
+		<label>
+			<input type="radio" name="color" value="red" /> 
+			Red <br />
+		</label>
+		<label>
+			<input type="radio" name="color" value="blue" /> 
+			Blue
+		</label>
+		<p>Hat to place on Player Two's head:</p>
+		<label>
+		<select name="selectedHat">
+			<option value="red">Red</option>
+			<option value="green">Green</option>
+			<option value="blue">Blue</option>
+		</select>
+		</label>
+		<div style={{ border: '1px solid black', padding: '10px', width: '200px', marginTop: '10px' }}>
+		 Have computer players make guesses <br /> exactly one will be correct
+		</div>
 	</div>
 );
 };
--- phase/src/pages/page6.js
+++ phase/src/pages/page6.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Page6 = () => {
 return (
 	<div>
-	<h1>TAB 6</h1>
+		<h1><bold>Lower Right</bold></h1>
+		<div style={{ 
+			position: 'absolute', 
+			bottom: '20px', 
+			right: '50px'
+		}}>
+			Down Here
+		</div>
 	</div>
 );
 };
