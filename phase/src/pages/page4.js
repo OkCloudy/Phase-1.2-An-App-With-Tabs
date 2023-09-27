1d42faf64c1ce55d05521a7d7cb36368e1316b36
Sean Sweeney <59295068+Sean-Sweeney@users.noreply.github.com>
2023-09-26 23:18:28 -0400

    Update page4.js

--- phase/src/pages/page4.js
+++ phase/src/pages/page4.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 
-const Page4 = () => {
-return (
-	<div>
-	<h1>TAB 4</h1>
-	</div>
-);
-};
+function Page4() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleImageClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <img
+        src="https://logowik.com/content/uploads/images/smiling-face-with-smiling-eyes-emoji5836.logowik.com.webp"
+        alt="Profile"
+        style={{ cursor: "pointer", width: "150px" }}
+        onClick={handleImageClick}
+      />
+      <h2 style={{ textAlign: "center", fontWeight: "bold" }}>
+        Smiley J Smile
+      </h2>
+
+      <Modal
+        show={showModal}
+        onHide={handleCloseModal}
+        centered
+		style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Notification</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Warning! Do not click profile picture!</Modal.Body>
+      </Modal>
+
+    </div>
+  );
+}
 
 export default Page4;
